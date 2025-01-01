ed0b522226ad773094c47ec843a4d06ee7fe3f74
yanagi1111 <yo0hovbrink@gmail.com>
2025-01-01 17:47:31 +0900

    ユーザー管理機能の実装３

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+                                      keys: [:nickname, :password, :password_confirmation, :last_name, :first_name,
+                                             :last_name_kana, :first_name_kana, :birthday])
   end
 end
