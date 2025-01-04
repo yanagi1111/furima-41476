54c31e27e91326e66ad11a78406b6a4676a10112
yanagi1111 <yo0hovbrink@gmail.com>
2025-01-04 21:32:57 +0900

    カテゴリー選択の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :preparation_day
+
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :preparation_day_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :preparation_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
 end
