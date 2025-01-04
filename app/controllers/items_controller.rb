7da000adcd8764e55a1facf0c9f640315f3b5f53
yanagi1111 <yo0hovbrink@gmail.com>
2025-01-04 22:32:39 +0900

    カテゴリー選択の実装２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -7,14 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to root_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :price,
-                                 :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :preparation_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
