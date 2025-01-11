c0ccddfa8a664652d547a355de91def59e20638d
yanagi1111 <yo0hovbrink@gmail.com>
2025-01-11 14:51:37 +0900

    商品情報編集画面の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+
   private
 
   def item_params
